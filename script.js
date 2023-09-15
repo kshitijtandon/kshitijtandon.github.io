e07ce346daa055cfdbe492581a834000c6769481
Kshitij <kshitijtandon1990@gmail.com>
2023-09-16 00:05:07 +1000

    updates

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-// Function to toggle active section when its title is clicked
+// Function to toggle the visibility of the text when a section heading is clicked
 document.querySelectorAll('h2').forEach(title => {
     title.addEventListener('click', function () {
         const section = this.parentElement;
-        const sections = document.querySelectorAll('.expandable-section');
+        const isOpen = section.style.maxHeight === 'none' || !section.style.maxHeight;
 
-        sections.forEach(s => s.classList.remove('active'));
-        section.classList.add('active');
+        // Close all sections
+        document.querySelectorAll('.expandable-section').forEach(s => {
+            s.style.maxHeight = '0';
+        });
+
+        // Open the clicked section if it was closed, otherwise close it
+        if (isOpen) {
+            section.style.maxHeight = section.scrollHeight + 'px';
+        }
     });
 });
